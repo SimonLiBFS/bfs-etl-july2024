9f263c93dce384d2f9e74bda6f18244cae6cb759
Boping Xia <xiaboping@gmail.com>
2024-07-13 17:03:37 -0400

    delete role

--- dags/s3_data_copy.py
+++ dags/s3_data_copy.py
@@ -30,8 +30,8 @@ with DAG(
         table='prestage_weather_team1',
         schema=SNOWFLAKE_SCHEMA,
         stage=SNOWFLAKE_STAGE,
-        role=SNOWFLAKE_ROLE,
         warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
         file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
             NULL_IF =('NULL','null',''), empty_field_as_null = true, FIELD_OPTIONALLY_ENCLOSED_BY = '\"' \
             ESCAPE_UNENCLOSED_FIELD = NONE RECORD_DELIMITER = '\n')''',
