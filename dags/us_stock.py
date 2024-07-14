23fe2fdde1baf86be598018b263dd9ef01a07d6e
kianakaslana648 <mc2582@georgetown.edu>
2024-07-14 17:27:28 -0400

    separate the dag into loading historical data (if no table exists) and incrementally loading the stock price at current day

--- dags/us_stock.py
+++ dags/us_stock.py
@@ -29,27 +29,30 @@ SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER'
 with DAG(
     "us_stock",
     start_date=datetime(2024, 7, 13),
-    schedule_interval=None,
+    end_date = datetime(2025, 7, 16),
+    schedule_interval='0 0 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire_june_de_team1'],
+    catchup=True,
 ) as dag:
 
     create_company_profile_table = SnowflakeOperator(
         task_id='create_company_profile_table',
         sql=f"""
         CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_COMPANY_PROFILE} AS
-        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_COMPANY_PROFILE} WHERE 1=0;
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_COMPANY_PROFILE};
         """,
         snowflake_conn_id=SNOWFLAKE_CONN_ID,
         warehouse=SNOWFLAKE_WAREHOUSE,
         role=SNOWFLAKE_ROLE,
     )
 
-    copy_company_profile_data = SnowflakeOperator(
-        task_id='copy_company_profile_data',
+    
+    create_symbols_table = SnowflakeOperator(
+        task_id='create_symbols_table',
         sql=f"""
-        INSERT INTO {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_COMPANY_PROFILE}
-        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_COMPANY_PROFILE};
+        CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_SYMBOLS} AS
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_SYMBOLS};
         """,
         snowflake_conn_id=SNOWFLAKE_CONN_ID,
         warehouse=SNOWFLAKE_WAREHOUSE,
@@ -60,45 +63,24 @@ with DAG(
         task_id='create_stock_history_table',
         sql=f"""
         CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_STOCK_HISTORY} AS
-        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_STOCK_HISTORY} WHERE 1=0;
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_STOCK_HISTORY}
+        WHERE DATE < {{ ds }};
         """,
         snowflake_conn_id=SNOWFLAKE_CONN_ID,
         warehouse=SNOWFLAKE_WAREHOUSE,
         role=SNOWFLAKE_ROLE,
     )
-
-    copy_stock_history_data = SnowflakeOperator(
-        task_id='copy_stock_history_data',
+    
+    load_stock_current_date = SnowflakeOperator(
+        task_id='load_stock_current_date',
         sql=f"""
         INSERT INTO {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_STOCK_HISTORY}
-        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_STOCK_HISTORY};
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_STOCK_HISTORY}
+        WHERE DATE = {{ ds }};
         """,
         snowflake_conn_id=SNOWFLAKE_CONN_ID,
         warehouse=SNOWFLAKE_WAREHOUSE,
         role=SNOWFLAKE_ROLE,
     )
-    
-    
-    create_symbols_table = SnowflakeOperator(
-        task_id='create_symbols_table',
-        sql=f"""
-        CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_SYMBOLS} AS
-        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_SYMBOLS} WHERE 1=0;
-        """,
-        snowflake_conn_id=SNOWFLAKE_CONN_ID,
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        role=SNOWFLAKE_ROLE,
-    )
-    
-    copy_symbols_data = SnowflakeOperator(
-        task_id='copy_symbols_data',
-        sql=f"""
-        INSERT INTO {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_SYMBOLS}
-        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_SYMBOLS};
-        """,
-        snowflake_conn_id=SNOWFLAKE_CONN_ID,
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        role=SNOWFLAKE_ROLE,
-    )
-    
-    create_company_profile_table >> copy_company_profile_data >> create_stock_history_table >> copy_stock_history_data >> create_symbols_table >> copy_symbols_data
+        
+    create_company_profile_table >> create_symbols_table >> create_stock_history_table >> load_stock_current_date
