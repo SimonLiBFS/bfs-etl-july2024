87257ac5fe12f9ef69e5f39172851c51b0d12db2
yashen0719 <yashen0719@gmail.com>
2024-07-14 11:33:32 -0400

    Update s3_data_copy_test_team2_updated.py

--- dags/s3_data_copy_test_team2_updated.py
+++ dags/s3_data_copy_test_team2_updated.py
@@ -19,7 +19,7 @@ with DAG(
     "s3_data_copy_test_team2",
     start_date=datetime(2024, 7, 13),
     end_date = datetime(2024, 7, 16),
-    schedule_interval='0 0 * * *',
+    schedule_interval='0 6 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire_airflow_team2'],
     catchup=True,
