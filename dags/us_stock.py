a6a25652975fb286dcce6502c8208c27598167b5
kianakaslana648 <mc2582@georgetown.edu>
2024-07-14 15:29:12 -0400

    add us stock dag

---
+++ dags/us_stock.py
@@ -0,0 +1,68 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.copy_into_snowflake import CopyFromExternalStageToSnowflakeOperator
+
+
+SNOWFLAKE_CONN_ID = 'snowflake_conn'
+
+SOURCE_DATABASE = 'US_STOCK_DAILY'
+SOURCE_SCHEMA = 'DCCM'
+SOURCE_TABLE_COMPANY_PROFILE = 'Company_Profile'
+SOURCE_TABLE_STOCK_HISTORY = 'Stock_History'
+SOURCE_TABLE_SYMBOLS = 'Symbols'
+
+
+TARGET_DATABASE = 'AIRFLOW0624'
+TARGET_SCHEMA = 'BF_DEV'
+TARGET_TABLE_COMPANY_PROFILE = 'DIM_COMPANY_PROFILE_TEAM1'
+TARGET_TABLE_STOCK_HISTORY = 'DIM_STOCK_HISTORY_TEAM1'
+TARGET_TABLE_SYMBOLS = 'DIM_SYMBOLS_TEAM1'
+
+SNOWFLAKE_ROLE = 'AW_developer'
+SNOWFLAKE_WAREHOUSE = 'aw_etl'
+SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER'
+
+
+with DAG(
+    "us_stock",
+    start_date=datetime(2024, 7, 13),
+    schedule_interval=None,
+    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+    tags=['beaconfire_june_de_team1'],
+) as dag:
+
+    copy_company_profile_table = SnowflakeOperator(
+        task_id='copy_company_profile_table',
+        sql=f"""
+        CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_COMPANY_PROFILE} AS
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_COMPANY_PROFILE} WHERE 1=0;
+        """,
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        role=SNOWFLAKE_ROLE,
+    )
+
+    copy_stock_history_table = SnowflakeOperator(
+        task_id='copy_stock_history_table',
+        sql=f"""
+        CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_STOCK_HISTORY} AS
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_STOCK_HISTORY} WHERE 1=0;
+        """,
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        role=SNOWFLAKE_ROLE,
+    )
+
+    copy_symbols_table = SnowflakeOperator(
+        task_id='copy_symbols_table',
+        sql=f"""
+        CREATE TABLE IF NOT EXISTS {TARGET_DATABASE}.{TARGET_SCHEMA}.{TARGET_TABLE_SYMBOLS} AS
+        SELECT * FROM {SOURCE_DATABASE}.{SOURCE_SCHEMA}.{SOURCE_TABLE_SYMBOLS} WHERE 1=0;
+        """,
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        role=SNOWFLAKE_ROLE,
+    )
