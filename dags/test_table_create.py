50bc72db920d84d82a5f16c3eeb83e2966652d65
gefuzkov <110410555+gefuzkov@users.noreply.github.com>
2024-07-14 16:45:57 -0700

    Update test_table_create.py
    update fact_stock_history table

--- dags/test_table_create.py
+++ dags/test_table_create.py
@@ -38,24 +38,20 @@ SELECT * FROM US_STOCK_DAILY.DCCM.Stock_History;
 INSERT INTO AIRFLOW0624.BF_DEV.dim_Symbols_TEAM2
 SELECT * FROM US_STOCK_DAILY.DCCM.Symbols;
 """
-#
-# ALTER_TABLE_SQL = """
-# CREATE OR REPLACE TASK update_target_fact_table_TEAM2
-#     warehouse = BF_ETL0624
-#     schedule = '1 day'
-# AS
-#     MERGE INTO "AIRFLOW0624"."BF_DEV"."DIM_STOCK_HISTORY_TEAM2" as t
-#     USING "US_STOCK_DAILY"."DCCM"."STOCK_HISTORY" as s
-#     ON t.SYMBOL = s.SYMBOL AND t.DATE = s.DATE
-#     WHEN MATCHED THEN
-#         UPDATE SET t.SYMBOL = s.SYMBOL,t.DATE = s.DATE,
-#                    t.OPEN = s.OPEN,t.HIGH = s.HIGH,
-#                    t.LOW = s.LOW,t.CLOSE = s.CLOSE,
-#                    t.VOLUME = s.VOLUME,t.ADJCLOSE = s.ADJCLOSE
-#     WHEN NOT MATCHED THEN
-#         INSERT (SYMBOL, DATE, OPEN, HIGH, LOW, CLOSE, VOLUME,ADJCLOSE)
-#         VALUES (s.SYMBOL, s.DATE, s.OPEN, s.HIGH, s.LOW, s.CLOSE, s.VOLUME,s.ADJCLOSE);
-# """
+
+ALTER_TABLE_SQL = """
+MERGE INTO "AIRFLOW0624"."BF_DEV"."DIM_STOCK_HISTORY_TEAM2" as t
+USING "US_STOCK_DAILY"."DCCM"."STOCK_HISTORY" as s
+ON t.SYMBOL = s.SYMBOL AND t.DATE = s.DATE
+WHEN MATCHED THEN
+        UPDATE SET t.SYMBOL = s.SYMBOL,t.DATE = s.DATE,
+                   t.OPEN = s.OPEN,t.HIGH = s.HIGH,
+                   t.LOW = s.LOW,t.CLOSE = s.CLOSE,
+                   t.VOLUME = s.VOLUME,t.ADJCLOSE = s.ADJCLOSE
+WHEN NOT MATCHED THEN
+        INSERT (SYMBOL, DATE, OPEN, HIGH, LOW, CLOSE, VOLUME,ADJCLOSE)
+        VALUES (s.SYMBOL, s.DATE, s.OPEN, s.HIGH, s.LOW, s.CLOSE, s.VOLUME,s.ADJCLOSE);
+"""
 
 
 with DAG(
@@ -77,13 +73,13 @@ with DAG(
         role=SNOWFLAKE_ROLE,
     )
 
-    # UPDATE_TABLE = SnowflakeOperator(
-    #     task_id='update_stock_table',
-    #     sql=ALTER_TABLE_SQL,
-    #     warehouse=SNOWFLAKE_WAREHOUSE,
-    #     database=SNOWFLAKE_DATABASE,
-    #     schema=SNOWFLAKE_SCHEMA,
-    #     role=SNOWFLAKE_ROLE,
-    #)
+    UPDATE_TABLE = SnowflakeOperator(
+        task_id='update_stock_table',
+        sql=ALTER_TABLE_SQL,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+        role=SNOWFLAKE_ROLE,
+    )
 
-    # CREATE_TABLE >> UPDATE_TABLE
+    CREATE_TABLE >> UPDATE_TABLE
